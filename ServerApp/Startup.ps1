f75a7d74c2060f1917dd6b607a938cad9cad2b8a
Nikita Chubko <nikitachubko@gmail.com>
2020-09-18 23:53:08 +0700

    DockerFile and Sturtup script added

---
+++ ServerApp/Startup.ps1
@@ -0,0 +1,6 @@
+﻿docker stop server
+docker rm server
+docker rmi expense_manager_server
+docker build -t expense_manager_server .
+docker run -d -p 5000:80 --name server expense_manager_server
+docker rmi $(docker images -f "dangling=true" -q)
